56b048c993ac20d3d8288eedaed4e77865481548
Paul Gerard <paulfgerard@hotmail.com>
2020-06-29 12:12:23 +0100

    Refinement of XML to CSV converter to use meaningful header labels.

--- Test/IcapPerformanceTest/XmlToCsvConverter/XmlToCsvConverter.py
+++ Test/IcapPerformanceTest/XmlToCsvConverter/XmlToCsvConverter.py
@@ -6,32 +6,34 @@ tree = ET.parse("../Results/FileRebuildResults.xml")
 root = tree.getroot()
 
 
-WANTED_ATTRIBUTES = [
-    "t",
-    "lt",
-    "ct",
-    "ts",
-    "s",
-    "lb",
-    "rc",
-    "rm",
-    "tn",
-    "by",
-    "sc",
-    "ec",
-    "ng",
-    "na"
+RECORDED_ATTRIBUTES = [
+    ["t", "Elapsed time (milliseconds)"],
+    ["lt", "Latency"],
+    ["ct", "ct"],
+    ["ts", "timeStamp"],
+    ["s", "Success flag"],
+    ["lb", "Label"],
+    ["rc", "Response Code"],
+    ["rm", "Response Message"],
+    ["tn", "Thread Name"],
+    ["by", "Bytes"],
+    ["sc", "Sample count"],
+    ["ec", "Error count"],
+    ["ng", "Number of active threads in this group"],
+    ["na", "	Number of active threads for all thread groups"],
 ]
+
+attribute_store = dict(RECORDED_ATTRIBUTES)
 
 with open("../Results/gw_metrics.csv", "w", newline="") as f:
     csvwriter = csv.writer(f)
 
     # Add column headers
     data = []
-    data.append(WANTED_ATTRIBUTES)
+    data.append(attribute_store.values())
     for http_sample_with_post in [item for item in root.findall("./httpSample") if item.attrib.get("lb").startswith("Post_")]:
         # add row
-        row = [http_sample_with_post.attrib[attrib] for attrib in WANTED_ATTRIBUTES]
+        row = [http_sample_with_post.attrib[attrib] for attrib in attribute_store.keys()]
         data.append(row)
 
     csvwriter.writerows(data)
